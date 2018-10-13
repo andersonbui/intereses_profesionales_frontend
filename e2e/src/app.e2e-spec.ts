51a37cc1e524ff812f20081ffb160d4e033fe727
Juan Manuel Díaz <juandiaz201093@gmail.com>
2018-10-13 15:10:48 -0500

    Cambio de proyecto

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to frontend-interpro!');
+    expect(page.getParagraphText()).toEqual('Welcome to interesesProfesionales!');
   });
 });
