3b483534971439642426a676bb8847437263c523
anderson <andersonbuitron@unicauca.edu.co>
2020-07-17 00:35:11 -0500

    actuazlizacion credenciales

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: '234234',
+    authDomain: 'werwerwer.firebaseapp.com',
+    databaseURL: 'https://werwerwer.firebaseio.com',
+    projectId: 'werwerwer',
+    storageBucket: 'werwerwer.appspot.com',
+    messagingSenderId: '48720534521108',
+    appId: "1:48720534521108"
+  }
+};
